2f9261e17be70bdeecdd25e17bc4e25b1fc9d8e4
augustomiguelfarias7-cmd <augustomiguelfarias7@gmail.com>
2025-11-21 10:40:42 -0300

    Implement ChatGusto and server functions
    Added ChatGusto and server functions to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,16 @@
 
+
 __version__ = "0.1.0"
 __author__ = "Augusto
+
+
+
+def ChatGusto():
+    print("chatgusto server api")
+    print("Meu pacote 'meupacotefoda' tá funcionando 100%!")
+
+def server ()():
+    return server_host 
+
+def chatgusto-resposta():
+  return respota
